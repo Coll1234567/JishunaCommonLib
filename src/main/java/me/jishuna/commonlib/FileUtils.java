c3965be6b20f4f14f443b87cc3e0f4a1191977f5
coll1234567 <joshl5324@gmail.com>
2021-04-24 17:32:33 -0400

    Initial commit

---
+++ src/main/java/me/jishuna/commonlib/FileUtils.java
@@ -0,0 +1,48 @@
+package me.jishuna.commonlib;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Optional;
+
+import javax.annotation.Nonnull;
+
+import org.bukkit.plugin.Plugin;
+
+import com.google.common.io.ByteStreams;
+
+public class FileUtils {
+
+	/**
+	 * Copies a file from a plugin jar into the plugins data folder. 
+	 * Preserves yml comments
+	 * 
+	 * @param source The source plugin
+	 * @param resourceName The name of the desired file, including extension.
+	 * @return an optional containing the file, or an empty optional if an error occurs.
+	 */
+
+	@Nonnull
+	public static Optional<File> copyResource(Plugin source, String resourceName) {
+		File folder = source.getDataFolder();
+		File resourceFile = new File(folder, resourceName);
+		if (!resourceFile.exists()) {
+			try {
+				resourceFile.createNewFile();
+				try (InputStream in = source.getResource(resourceName);
+						OutputStream out = new FileOutputStream(resourceFile)) {
+					ByteStreams.copy(in, out);
+				}
+
+			} catch (IOException ex) {
+				source.getLogger().severe(
+						"Encountered " + ex.getClass().getSimpleName() + " while copying file " + resourceName + ".");
+				ex.printStackTrace();
+				return Optional.empty();
+			}
+		}
+		return Optional.of(resourceFile);
+	}
+}
