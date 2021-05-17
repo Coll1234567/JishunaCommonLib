2bad7925fb8c245eaa6fefba1ac72bc43229def4
coll1234567 <joshl5324@gmail.com>
2021-05-17 16:11:47 -0400

    toRomanNumeral, MessageConfig

---
+++ src/main/java/me/jishuna/commonlib/MessageConfig.java
@@ -0,0 +1,46 @@
+package me.jishuna.commonlib;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class MessageConfig {
+
+	private Map<String, String> stringMap = new HashMap<>();
+	private Map<String, List<String>> stringListMap = new HashMap<>();
+
+	private final YamlConfiguration configuration;
+
+	public MessageConfig(YamlConfiguration configuration) {
+		this.configuration = configuration;
+	}
+
+	public void refresh() {
+		this.stringMap.clear();
+		this.stringListMap.clear();
+	}
+
+	public String getString(String key) {
+		return this.stringMap.computeIfAbsent(key,
+				mapKey -> ChatColor.translateAlternateColorCodes('&', this.configuration.getString(key, "")));
+	}
+
+	public List<String> getStringList(String key) {
+		return this.stringListMap.computeIfAbsent(key, mapKey -> parseStringList(key));
+	}
+
+	private List<String> parseStringList(String key) {
+		List<String> list = new ArrayList<>();
+
+		for (String string : this.configuration.getStringList(key)) {
+			list.add(ChatColor.translateAlternateColorCodes('&', string));
+		}
+		return list;
+	}
+
+}
--- src/main/java/me/jishuna/commonlib/StringUtils.java
+++ src/main/java/me/jishuna/commonlib/StringUtils.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,6 +12,24 @@ import org.bukkit.ChatColor;
 
 public class StringUtils {
 	private static Map<Integer, Pattern> regexCache = new HashMap<>();
+
+	private final static TreeMap<Integer, String> romanMap = new TreeMap<Integer, String>();
+
+	static {
+		romanMap.put(1000, "M");
+		romanMap.put(900, "CM");
+		romanMap.put(500, "D");
+		romanMap.put(400, "CD");
+		romanMap.put(100, "C");
+		romanMap.put(90, "XC");
+		romanMap.put(50, "L");
+		romanMap.put(40, "XL");
+		romanMap.put(10, "X");
+		romanMap.put(9, "IX");
+		romanMap.put(5, "V");
+		romanMap.put(4, "IV");
+		romanMap.put(1, "I");
+	}
 
 	public static List<String> splitString(String string, int size) {
 		Pattern pattern = regexCache.computeIfAbsent(size,
@@ -27,6 +46,14 @@ public class StringUtils {
 			prev = next;
 		}
 		return result;
+	}
+
+	public static final String toRomanNumeral(int number) {
+		int l = romanMap.floorKey(number);
+		if (number == l) {
+			return romanMap.get(number);
+		}
+		return romanMap.get(l) + toRomanNumeral(number - l);
 	}
 
 }
