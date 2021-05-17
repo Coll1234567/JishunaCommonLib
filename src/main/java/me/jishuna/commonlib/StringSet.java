13a71c967679eff5afb7264ca29c9923a7dddc28
coll1234567 <joshl5324@gmail.com>
2021-05-17 17:45:50 -0400

    String set PDC type

---
+++ src/main/java/me/jishuna/commonlib/StringSet.java
@@ -0,0 +1,9 @@
+package me.jishuna.commonlib;
+
+import java.io.Serializable;
+import java.util.HashSet;
+
+public class StringSet extends HashSet<String> implements Serializable {
+	private static final long serialVersionUID = 4845312591984707383L;
+
+}
